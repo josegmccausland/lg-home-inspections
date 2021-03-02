2901cf2ee82e6084098ab1b8b427a5df92b4cc41
josegmccausland <ajgarcia3@mail.usf.edu>
2021-03-01 20:57:52 -0500

    trying to fix initial render issue

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
-import { HashRouter } from 'react-router-dom'
+// import { HashRouter } from 'react-router-dom'
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root'),
-  <HashRouter>
-    <App />
-  </HashRouter>,
+  // document.getElementById('root'),
+  // <HashRouter>
+  //   <App />
+  // </HashRouter>,
   document.getElementById('root')
 )
 
